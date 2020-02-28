b14b333eab5cf7a1904594d32cc97a9dca4ce813
linkcube <poohbah125@gmail.com>
2020-02-28 12:39:29 -0800

    initial split

---
+++ README.md
@@ -0,0 +1,40 @@
+# Svelte-preview-card
+
+Basic card presenting a title, sub-text, and background image ideally for a grid layout. Also has an event emitter for when it has been clicked.
+
+Demo: http://linkcube.github.io/
+
+## Styling
+
+This part is going to be fairly rough until I figure out a standard for themeing across my components, but for now it exposes raw color values for the text and background states.
+
+## Usage
+
+```
+let src = "my picture";
+<PreviewCard
+    background_source={src}
+    primary_text="Title"
+    sub_text="My Picture"
+></PreviewCard>
+```
+
+Exposed Events:
+Event | Description
+-- | --
+`on:open` | When the card is clicked
+
+Optional settings:
+Param | Description | Type
+--- | --- | ---
+`background_source` | Source for the card's image, defaults to none | String
+`primary_text` `sub_text` | Displayed text values, if these are not used the image max-height is increased. | String
+`alt_text` | Alt text for the image | String
+`disabled` | Whether to disable the card from being clicked | Boolean
+`background_color` `primary_text_color` `sub_text_color` | Basic color values | String
+`active_color` | Background color of the card when the mouse is held down on it | String
+`focus_color` | Background color of the card if clicked | String
+
+## Links
+
+[Github](https://github.com/Linkcube/svelte-preview-card)
---
+++ src/Component.svelte
@@ -0,0 +1,110 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+    export let background_source = false;
+    export let primary_text = false;
+    export let sub_text = false;
+    export let alt_text = "preview_image";
+    export let background_color = "white";
+    export let active_color = "lightgrey";
+    export let focus_color = background_color;
+    export let primary_text_color = "black";
+    export let sub_text_color = "lightgrey";
+    export let disabled = false;
+
+    const dispatch = createEventDispatcher();
+
+    let max_height = 180;
+    if (!primary_text) max_height += 20;
+    if (!sub_text) max_height += 20;
+    max_height = `${max_height}px`;
+    if (disabled) active_color = 0;
+</script>
+
+<style>
+    .card {
+        height: 250px;
+        width: 220px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        margin: 10px;
+        transition-duration: 500ms;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.1);
+        cursor: pointer;
+        border: none;
+        outline:none;
+        background: var(--background_color)
+        
+    }
+
+    .card:hover {
+        transition-duration: 500ms;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.4);
+    }
+
+    .card:focus {
+        background: var(--focus_color);
+    }
+
+    .card:active {
+        background: var(--active_color)
+    }
+
+    .card::-moz-focus-inner {
+        border: 0;
+    }
+
+    img {
+        display: flex;
+        max-width: 180px;
+        max-height: var(--height);
+        transition-duration: 500ms;
+        margin-left: auto;
+        margin-right: auto;
+        opacity: .9;
+        outline: 1px solid transparent;
+    }
+
+    .card:hover img {
+        opacity: 1;
+        transition-duration: 500ms;
+        outline: 1px solid transparent;
+    }
+
+    .primary_text {
+        text-align: center;
+        color: var(--primary-color);
+        font-size: 20px;
+    }
+
+    .date {
+        text-align: center;
+        color: var(--primary-color);
+    }
+
+    .footer {
+        margin: auto;
+    }
+
+    .hover {
+        margin-top: auto;
+        margin-bottom: auto;
+    }
+
+</style>
+
+<button class="card" style="--background_color:{background_color};--active_color:{active_color};--focus_color:{focus_color}" on:click={() => dispatch('open')} {disabled}>
+    {#if background_source !== false}
+        <div class="hover">
+            <img src={background_source} alt={alt_text} style="--height:{max_height}">
+        </div>
+    {/if}
+    <div class="footer">
+        {#if primary_text}
+            <div class="primary_text" style="--primary-color:{primary_text_color}">{primary_text}</div>
+        {/if}
+        {#if sub_text}
+            <div class="date" style="--primary-color:{sub_text_color}">{sub_text}</div>
+        {/if}
+    </div>
+</button>
